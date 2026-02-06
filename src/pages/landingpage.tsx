b3f5f47ad58f8014d095be0c78d818e233dba552
Mzach55 <mikezach37@gmail.com>
2026-02-06 16:45:44 -0500

    create sample landing page w/ mantine bootstrap

---
+++ src/pages/landingpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '@/components/Navbar';
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is where you can introduce your product or service.</p>
+    </div>
+  );
+}
