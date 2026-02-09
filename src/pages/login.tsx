255b596e6ab9204840ce1cea8ead14b623504bd0
s0dl <tui91919@temple.edu>
2026-02-09 21:50:00 +0000

    Added initial login and signup

---
+++ src/pages/login.tsx
@@ -0,0 +1,61 @@
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function SignUp() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async () => {
+    const { data, error } = await authClient.signIn.email({
+        /**
+         * The user email
+         */
+        email,
+        /**
+         * The user password
+         */
+        password,
+        /**
+         * A URL to redirect to after the user verifies their email (optional)
+         */
+        callbackURL: "/dashboard",
+        /**
+         * remember the user session after the browser is closed. 
+         * @default true
+         */
+        rememberMe: false
+    }, {
+        //callbacks
+    })
+  }
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleLogin();
+      }}
+    >
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit" disabled={loading}>
+        {loading ? "Creating account..." : "Sign up"}
+      </button>
+    </form>
+  );
+}
---
+++ src/pages/signup.tsx
@@ -0,0 +1,65 @@
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function SignUp() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSignUp = async () => {
+    const { data, error } = await authClient.signUp.email({
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/dashboard" // A URL to redirect to after the user verifies their email (optional)
+    }, {
+        onRequest: (ctx) => {
+            //show loading
+        },
+        onSuccess: (ctx) => {
+            //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+            // display the error message
+            alert(ctx.error.message);
+        },
+    });
+  }
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSignUp();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit" disabled={loading}>
+        {loading ? "Creating account..." : "Sign up"}
+      </button>
+    </form>
+  );
+}
