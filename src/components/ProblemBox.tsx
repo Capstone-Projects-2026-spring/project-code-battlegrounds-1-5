d42ebfd23ab4716703666448bbafe69568763727
ElanReizas <elanreizas@gmail.com>
2026-02-22 19:09:18 -0500

    Hardcoded problem
    Grabbed problems and put them into the problem box

--- src/components/ProblemBox.tsx
+++ src/components/ProblemBox.tsx
@@ -4,25 +4,23 @@ export default function ProblemBox() {
   return (
     // Remove shadow and use h="100%" to fill the parent Box
     <Paper p="md" h="100%" bg="transparent">
-        {/* Remove the width: "20%" here! Let the parent handle width. */}
-        <ScrollArea h="100%" offsetScrollbars>
-            <Stack gap="md">
-                <Title order={3}>
-                    Problem Title
-                </Title>
-                <Text size="sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                    laboris nisi ut aliquip ex ea commodo consequat.
-                </Text>
-                <Text size="sm">
-                    {/* Add enough text here to test the scroll */}
-                    More content to ensure scrolling works correctly within the 
-                    allocated vertical space...
-                </Text>
-            </Stack>
-        </ScrollArea>
+      {/* Remove the width: "20%" here! Let the parent handle width. */}
+      <ScrollArea h="100%" offsetScrollbars>
+        <Stack gap="md">
+          <Title order={3}>Two Sum</Title>
+          <Text size="sm">
+            Given an array of integers nums¬¨‚Ä†and an integer target, return
+            indices of the two numbers such that they add up to target. You may
+            assume that each input would have exactly one solution, and you may
+            not use the same element twice. You can return the answer in any
+            order.
+          </Text>
+          <Text size="sm">
+            {/* Add enough text here to test the scroll */}
+            Array,Hash Table
+          </Text>
+        </Stack>
+      </ScrollArea>
     </Paper>
   );
 }
