c7c31f8e8d1df19abe9ac16d4431a27ba6b6e020
samirbuch <samir.buch@temple.edu>
2026-03-10 13:03:31 -0400

    auth: improve visual

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,17 +1,25 @@
-
-import Router from "next/router";
+import { useRouter } from "next/router";
+import { Button, Flex, Card } from "@mantine/core";
 
 export default function AuthPage() {
-
+  const router = useRouter();
 
   return (
-    <>
-      <div>
-        <div>
-          <button onClick={() => Router.push("/login")}>Login</button>
-          <button onClick={() => Router.push("/signup")}>Sign Up</button>
-        </div>
-      </div>
-    </>
+    <Flex justify="center" align="center" mih="100vh">
+      <Card miw="30%">
+        <Flex direction={"column"} gap="xs">
+          <Button
+            onClick={() => router.push("/login")}
+          >
+            Log in
+          </Button>
+          <Button
+            onClick={() => router.push("/signup")}
+          >
+            Sign up
+          </Button>
+        </Flex>
+      </Card>
+    </Flex>
   );
 }
