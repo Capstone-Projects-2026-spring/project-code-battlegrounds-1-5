ba29fe372158843cce0f2b311a73f671144ba68a
samirbuch <samir.buch@temple.edu>
2026-03-20 22:23:31 -0400

    install posthog with basic instrumentation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,34 @@
 import "@mantine/core/styles.css";
-
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
+import type { AppProps } from "next/app";
 import { createTheme, MantineProvider } from "@mantine/core";
+import { useEffect } from "react";
+import { Router } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react"
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: '2026-01-30',
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+  }, []);
+
   return (
-    <MantineProvider theme={theme} defaultColorScheme="auto">
-      <Component {...pageProps} />
-    </MantineProvider>
+    <PostHogProvider client={posthog}>
+      <MantineProvider theme={theme} defaultColorScheme="auto">
+        <Component {...pageProps} />
+      </MantineProvider>
+    </PostHogProvider>
   );
 }
