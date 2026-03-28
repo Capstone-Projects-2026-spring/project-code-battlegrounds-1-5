92934229ae2eab69d5dbeebb5253a221e61383cf
samirbuch <samir.buch@temple.edu>
2026-03-28 19:10:51 -0400

    redesigned test cases first pass

--- src/components/GameTestCase.tsx
+++ src/components/GameTestCase.tsx
@@ -1,3 +1,58 @@
-export default function GameTestCase() {
-  
+import { type ParameterType } from "@/lib/ProblemInputOutput";
+import { Button, Stack, Table, Text, TextInput } from "@mantine/core";
+import { IconPlayerPlay } from "@tabler/icons-react";
+
+export interface TestableCase {
+  id: number,
+  functionInput: ParameterType[],
+  expectedOutput: ParameterType
+}
+
+export interface GameTestCaseProps {
+  testableCase: TestableCase,
+  onTestCaseChange: (test: TestableCase) => void;
+}
+
+export default function GameTestCase(props: GameTestCaseProps) {
+  const { testableCase } = props;
+
+  return (
+    <Stack gap="md">
+      <Table>
+        <Table.Tbody>
+          {testableCase.functionInput.map((input, idx) => (
+            <Table.Tr key={idx}>
+              <Table.Td align="right">
+                <Text c="dimmed">
+                  {input.name} =
+                </Text>
+              </Table.Td>
+              <Table.Td>
+                <TextInput />
+              </Table.Td>
+            </Table.Tr>
+          ))}
+
+          <Table.Tr>
+            <Table.Td align="right">
+              <Text c="dimmed">
+                Output =
+              </Text>
+            </Table.Td>
+            <Table.Td>
+              <TextInput />
+            </Table.Td>
+          </Table.Tr>
+        </Table.Tbody>
+      </Table>
+
+      <Button
+        color="green"
+        rightSection={<IconPlayerPlay />}
+        style={{ alignSelf: "flex-start" }}
+      >
+        RUN
+      </Button>
+    </Stack>
+  )
 }
