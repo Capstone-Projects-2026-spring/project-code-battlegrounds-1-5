e929001c74370bb205fcc64ae9ad79287f4c4579
samirbuch <samir.buch@temple.edu>
2026-04-04 13:46:57 -0400

    auth: brand

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,14 +1,18 @@
 import { useRouter } from "next/router";
 import { Button, Flex, Card } from "@mantine/core";
+import Brand from "@/components/Brand";
 
 export default function AuthPage() {
   const router = useRouter();
 
   return (
     <Flex justify="center" align="center" mih="100vh">
-      <Card miw="30%">
+      <Card miw="30%" shadow="xl" withBorder>
         <Flex direction={"column"} gap="xs">
+          <Brand />
+          
           <Button
+            mt="md"
             onClick={() => router.push("/login")}
           >
             Log in
