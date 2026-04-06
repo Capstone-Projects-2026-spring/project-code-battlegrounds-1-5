cfea0e9e217f260ef1ef890713cfc1652a84a640
Saad Chaudry <tui91919@temple.edu>
2026-04-06 12:39:59 -0400

    migration changes

--- prisma/migrations/20260406162933_changed_game_results/migration.sql
+++ prisma/migrations/20260406162933_changed_game_results/migration.sql
@@ -17,5 +17,27 @@ ADD COLUMN     "team2Code" TEXT,
 ADD COLUMN     "team2TimeToPassMs" INTEGER,
 ALTER COLUMN "winningTeamId" DROP NOT NULL;
 
+-- AlterTable
+ALTER TABLE "Team" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE "Party" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Party_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PartyMember" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "partyId" TEXT NOT NULL,
+
+    CONSTRAINT "PartyMember_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "PartyMember" ADD CONSTRAINT "PartyMember_partyId_fkey" FOREIGN KEY ("partyId") REFERENCES "Party"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
 -- AddForeignKey
 ALTER TABLE "GameResult" ADD CONSTRAINT "GameResult_winningTeamId_fkey" FOREIGN KEY ("winningTeamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
