308e3fa35084c7989669aed5cb1fb8d0559b3beb
samirbuch <samir.buch@temple.edu>
2026-04-10 16:06:51 -0400

    Navbar: remove header links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Burger, Container, Group, Text, Anchor } from '@mantine/core';
+import { Burger, Container, Group, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import Link from 'next/link'; // <-- Import Next.js Link
 import classes from '../styles/comps/Header.module.css';
@@ -11,7 +11,7 @@ interface HeaderProps {
   isSpectator?: boolean
 }
 
-export default function HeaderSimple(props: HeaderProps) {
+export default function Navbar(props: HeaderProps) {
   const [opened, { toggle }] = useDisclosure(false);
   const [active, setActive] = useState(props.links[0]);
 
@@ -22,35 +22,12 @@ export default function HeaderSimple(props: HeaderProps) {
   // put the remaining info back together.
   const gameInfo = titleParts.slice(1).join('|'); // " GAMEMODE: ... | YOUR ROLE: ..."
 
-  const items = props.links.map((link) => (
-    <Anchor
-      key={link}
-      className={classes.link}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link);
-      }}
-    >
-      {link}
-    </Anchor>
-  ));
-
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
 
         <Brand blink />
         <Text fw={600} mr="auto">
-          {/* mantine anchor tag instead a <a or whatever else we'd use  */}
-          {/* <Anchor
-            component={Link}
-            href="/"
-            underline="hover"
-            fw={800}
-            style={{ letterSpacing: '1px' }}
-          >
-            {brandName}
-          </Anchor> */}
 
           {/* Remaining status text */}
           {gameInfo && <span style={{ opacity: 0.8, fontWeight: 400 }}> | {gameInfo}</span>}
@@ -61,10 +38,6 @@ export default function HeaderSimple(props: HeaderProps) {
             </span>
           )}
         </Text>
-
-        <Group gap={6} visibleFrom="xs">
-          {items}
-        </Group>
 
         <Burger
           opened={opened}
