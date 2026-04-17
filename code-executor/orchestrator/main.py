5d0ab4533eac0664dcac9b2a4c421a187e42511f
Julia Fasick <juli4fasick@gmail.com>
2026-04-16 21:48:01 -0400

    fix typo in startup script

--- code-executor/orchestrator/main.py
+++ code-executor/orchestrator/main.py
@@ -79,7 +79,7 @@ class VMProvisioner:
                 metadata.items = [
                     {
                         "key":"startup-script", # TODO: MUST REMOVE THIS GIT SWITCH AS WE MERGE. vm is already on main. while i test, i want to be on my branch
-                        "value":"#!/bin/bash\ncd /home/juli4fasick/project-code-battlegrounds-1-5\ngit switch /feat/vm-orchestrator-fr\ngit pull\nsource ./.venv/bin/activate\ncd ./code-executor\npip3 install -r requirements.txt\ncd ./executor-api\nfastapi run",
+                        "value":"#!/bin/bash\ncd /home/juli4fasick/project-code-battlegrounds-1-5\ngit switch feat/vm-orchestrator-fr\ngit pull\nsource ./.venv/bin/activate\ncd ./code-executor\npip3 install -r requirements.txt\ncd ./executor-api\nfastapi run",
                     }
                 ]
                 instance.metadata = metadata
