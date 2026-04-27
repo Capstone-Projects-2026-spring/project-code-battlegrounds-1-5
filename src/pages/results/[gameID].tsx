f2911b600d90a978c40a205b3bcc568390153c3e
samirbuch <samir.buch@temple.edu>
2026-04-27 10:56:21 -0400

    .

--- src/pages/results/[gameID].tsx
+++ src/pages/results/[gameID].tsx
@@ -106,7 +106,7 @@ export default function Page() {
   // if we aren't even logged in
   useEffect(() => {
     if (!isPending && !session) {
-      router.replace("/auth");
+      router.replace("/login");
     }
   }, [isPending, session, router]);
   return <Results />;
